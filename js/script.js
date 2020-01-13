a8a5decff8f50db43f2b965148f98de085ae2106
Noel Thomson <noel.thomson@gmail.com>
2020-01-13 18:10:39 -0500

    first commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,47 @@
-var mobileToggle = document.querySelector(".top-nav__mobile-toggle");
-var header = document.querySelector("header");
-var lastScrollTop = 0;
+const mobileToggle = document.querySelector(".top-nav__mobile-toggle");
+const header = document.querySelector("header");
+const filters = document.querySelectorAll(".filter");
+
+let currentFilter = document.querySelector(".filter.current");
+
+// var lastScrollTop = 0;
 
 mobileToggle.addEventListener("click", function() {
   header.classList.toggle("open");
+});
+
+for (const el of filters) {
+  el.addEventListener("click", function() {
+    currentFilter.classList.remove("current"); 
+    this.classList.add("current");
+    currentFilter = this;
+  });
+}
+
+$(function(){
+  
+  $(".portfolio__items").isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 1500,
+      easing: 'linear',
+      queue: false
+    }
+  });
+
+  $("#filter__buttons a").click(function() {
+    let selector = $(this).attr("data-filter");
+    
+    $(".portfolio__items").isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 1500,
+        easing: 'linear',
+        queue: false
+      }
+    });
+
+    return false;
+
+  });
 });
