3960d0ac2b65f9b83c35b7ac806e1adfd40ed0e1
Noel Thomson <noel.thomson@gmail.com>
2020-10-27 16:40:57 -0400

    particle js

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,27 @@ var browserSync = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
 
 // compile scss into css
-function style(){
+function style() {
   // 1. locate scss file
-  return gulp.src('./scss/**/*.scss')
-  // 2. convert to css file through sass compiler
-    .pipe(sass().on('error', sass.logError))
-  // 2.1 pass through autoprefixer
-    .pipe(autoprefixer())
-  // 3. location to save compiled css
-    .pipe(gulp.dest('./css'))
-  // 4. stream changes to browsers
-    .pipe(browserSync.stream());
+  return (
+    gulp
+      .src('./scss/**/*.scss')
+      // 2. convert to css file through sass compiler
+      .pipe(sass().on('error', sass.logError))
+      // 2.1 pass through autoprefixer
+      .pipe(autoprefixer())
+      // 3. location to save compiled css
+      .pipe(gulp.dest('./css'))
+      // 4. stream changes to browsers
+      .pipe(browserSync.stream())
+  );
 }
 
-function watch(){
+function watch() {
   browserSync.init({
     server: {
-      baseDir: './'
-    }
+      baseDir: './',
+    },
   });
   gulp.watch('./scss/**/*.scss', style);
   gulp.watch('./**/*html').on('change', browserSync.reload);
