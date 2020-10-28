cc3bdce9953f74abc060d990d5d3e8eeb9c91d9a
Noel Thomson <noel.thomson@gmail.com>
2020-10-28 19:41:18 -0400

    header and portfolio ui updates

--- js/script.js
+++ js/script.js
@@ -1,62 +1,62 @@
-const mobileToggle = document.querySelector(".top-nav__mobile-toggle");
-const header = document.querySelector("header");
-const filters = document.querySelectorAll(".filter");
-const navLinks = document.querySelectorAll(".top-nav__link");
+const mobileToggle = document.querySelector('.top-nav__mobile-toggle');
+const header = document.querySelector('header');
+const filters = document.querySelectorAll('.filter');
+const navLinks = document.querySelectorAll('.top-nav__link');
 
-let currentFilter = document.querySelector(".filter.current");
+let currentFilter = document.querySelector('.filter.current');
 
 // var lastScrollTop = 0;
 
-mobileToggle.addEventListener("click", function() {
-  header.classList.toggle("open");
+mobileToggle.addEventListener('click', function () {
+  header.classList.toggle('open');
 });
 
 for (const navLink of navLinks) {
-  navLink.addEventListener("click", function() {
-    if (header.classList.contains("open")) {
-      header.classList.toggle("open");
+  navLink.addEventListener('click', function () {
+    if (header.classList.contains('open')) {
+      header.classList.toggle('open');
     }
   });
 }
 
 for (const el of filters) {
-  el.addEventListener("click", function() {
-    currentFilter.classList.remove("current");
-    this.classList.add("current");
+  el.addEventListener('click', function () {
+    currentFilter.classList.remove('current');
+    this.classList.add('current');
     currentFilter = this;
   });
 }
 
-$(function() {
-  $(".portfolio__items").isotope({
-    filter: "*",
-    layoutMode: "fitRows",
+$(function () {
+  $('.portfolio__items').isotope({
+    filter: '*',
+    layoutMode: 'fitRows',
     animationOptions: {
       duration: 1500,
-      easing: "linear"
+      easing: 'linear',
       // queue: false
-    }
+    },
   });
 
-  $("#filter__buttons a").click(function() {
-    let selector = $(this).attr("data-filter");
+  $('#filter__buttons a').click(function () {
+    let selector = $(this).attr('data-filter');
 
-    $(".portfolio__items").isotope({
+    $('.portfolio__items').isotope({
       filter: selector,
-      layoutMode: "fitRows",
+      layoutMode: 'fitRows',
       animationOptions: {
         duration: 1500,
-        easing: "linear"
+        easing: 'linear',
         // queue: false
-      }
+      },
     });
 
     return false;
   });
 });
 
-$(".portfolio__items")
+$('.portfolio__items')
   .imagesLoaded()
-  .progress(function() {
-    $(".portfolio__items").isotope("layout");
+  .progress(function () {
+    $('.portfolio__items').isotope('layout');
   });
