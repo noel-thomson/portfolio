de2c0c3c95edbdb7d79daad0990569c17ed05bec
Noel Thomson <noel.thomson@gmail.com>
2020-12-07 15:07:30 -0500

    portfolio

--- src/js/modalCopy.js
+++ src/js/modalCopy.js
@@ -2,64 +2,81 @@ const modalCopy = [
   {
     title: 'Apple Landing Page',
     body:
-      'After extracting the image assets for this webpage, I challenged myself to reproduce a pixel perfect replica as though I had been asked to convert the site from a photoshop file. Flexbox, absolute positioning, and media queries were essential for this task as unique images are inserted at different device screen sizes and the grid system is reordered.',
+      'I extracted the image assets for this webpage and reproduced a pixel perfect replica as though I had been asked to convert the site from a photoshop file. Unique images are inserted at different device widths and grid sections are reordered on mobile.',
     project: 'https://noel-thomson.github.io/apple-landing/',
     code: 'https://github.com/noel-thomson/apple-landing/',
+    id: 0,
   },
   {
     title: 'E-Commerce Design',
     body:
-      'For this project, I set out to create a design that felt modern but looked classic. The contact form was hand coded from scratch with animations inspired by Google Material design. The Slick Slider library was customized to fit my needs and Google Maps was integrated at the bottom. The site is carefully optimized to maintain a sleek aesthetic across all devices.',
+      "An original design that is both modern and classic, inspired by Shopify's design language. The contact form was created with animations inspired by Google Material design, the Slick Slider library was customized and the Google Maps api was integrated.",
     project: 'https://noel-thomson.github.io/business-template/',
     code: 'https://github.com/noel-thomson/business-template/',
+    id: 1,
+  },
+  {
+    title: 'Westwood Financial',
+    body:
+      'A webpage for a local Tax and Accounting firm. The design is modern, approachable and effective at converting leads through the contact form. Bootstrap was utilized for additional style and structure.',
+    project: 'https://westwood-financial.co',
+    code: 'https://github.com/noel-thomson/westwood-financial/',
+    id: 2,
   },
   {
     title: 'Digital Agency',
     body:
-      "This project features a variety of page animations to showcase the modern design features that a digital agency can provide. A careful combination of transform, opacity, keyframe and set interval functions were combined to craft fluid hero and showcase section animations. JavaScript's scroll offset API is used to dynamically control the animation of the navigation menu, and AOS was incorporated for fade effects.",
+      'A webpage featuring custom animations to showcase modern design language that a digital agency can provide. Animations are achieved through a combination of Sass and JavaScript including transitions, keyframes and setInterval functions.',
     project: 'https://noel-thomson.github.io/digital-agency/',
     code: 'https://github.com/noel-thomson/digital-agency/',
+    id: 3,
   },
   {
     title: 'Movie Search Database',
     body:
-      "This project uses asynchronous JavaScript to access data from the OMDB API and dynamically render this data in the form of an intuitive user interface. Clicking on a movie takes the user to a details page displaying key movie information. Keyword input returns ten results and Twitter's CSS Bootstrap library was incorporated for layout and style.",
+      'This project queries the OMDB API and dynamically renders it in the form of an intuitive user interface. Clicking on a movie takes the user to a details page displaying key information. Bootstrap was incorporated for additional style and layout.',
     project: 'https://noel-thomson.github.io/movie-database-app/',
     code: 'https://github.com/noel-thomson/movie-database-app/',
+    id: 4,
   },
   {
     title: 'Blackjack',
     body:
-      "This is a JavaScript focused project where game logic was constructed true to the rules of the game with an algorithm to adjust player scores based on the variable nature of the Ace, declare a winner, and incorporate dynamic animations and audio effects. Dom manipulation is used to display a history of each player's drawn cards.",
+      'Game logic is constructed with JavaScript, true to the rules of the game. A virtual deck of cards is combined with an algorithm to determine outcomes and a winner. Dynamic animations, audio effects and DOM manipulation are incorporated.',
     project: 'https://noel-thomson.github.io/blackjack-app/',
     code: 'https://github.com/noel-thomson/blackjack-app/',
+    id: 5,
   },
   {
     title: 'E-Commerce Store',
     body:
-      "This project is a full-stack application build with ReactJS and ExpressJS connected to MongoDB's cloud-hosted database. Redux is incorporated for state management and Mongoose for database model schemas. Features including shopping cart management, user authentication and permissions are in the process of completion.",
+      "This project is a full-stack application build with ReactJS and ExpressJS connected to MongoDB's cloud-hosted database. Redux is incorporated for state management and Mongoose for database model schemas. Further features with include shopping cart management, user authentication and permissions.",
     project: 'https://mern-pro-shop.herokuapp.com/',
     code: 'https://github.com/noel-thomson/mern-ecommerce-store',
+    id: 6,
   },
   {
     title: 'Photography Generator',
     body:
-      'This project uses asynchronous JavaScript to access HD photography data from the Pexels API and dynamically render it in a responsive grid format. Keywords return fifteen results, and suggested search terms below the search bar return API queries as well.',
+      'This project queries the Pexels API for HD photography and dynamically renders it in a responsive grid format.',
     project: 'https://noel-thomson.github.io/stock-photography-generator/',
     code: 'https://github.com/noel-thomson/stock-photography-generator/',
+    id: 7,
   },
-  {
-    title: 'React Calculator',
-    body:
-      "Modeled after Apple's calculator, this project uses ReactJS and JavaScript to parse keyboard or click input and calculate results to two decimal places.",
-    project: 'https://mystifying-jang-12ffda.netlify.app/',
-    code: 'https://github.com/noel-thomson/react-calculator',
-  },
+  // {
+  //   title: 'React Calculator',
+  //   body:
+  //     "Modeled after Apple's calculator, this project uses ReactJS and JavaScript to parse user input and calculate results to two decimal places.",
+  //   project: 'https://mystifying-jang-12ffda.netlify.app/',
+  //   code: 'https://github.com/noel-thomson/react-calculator',
+  //   id: 8,
+  // },
   {
     title: 'Pomodoro Timer',
     body:
-      'A popular time management tool, this timer uses React UseState and UseEffect Hooks to dynamically render the user interface, oscillating between user defined periods of focused work and short breaks.',
+      'A popular time management tool, this timer uses React UseState and UseEffect Hooks to dynamically render a clock that oscillates periods of focused work and breaks.',
     project: 'https://festive-blackwell-0f363b.netlify.app/',
     code: 'https://github.com/noel-thomson/pomodoro-timer',
+    id: 9,
   },
 ];
